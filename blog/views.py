3197246c15a60e45008fbe8b021da36ad44c4180
JS <jasdepw@gmail.com>
2018-10-31 13:57:35 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+#models.py에 정의된 모델을 가져옴
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
