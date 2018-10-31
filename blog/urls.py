651706fe5f9e80a24be7268ce44615fa2b616f8c
JS <jasdepw@gmail.com>
2018-10-31 19:17:49 +0900

    Added view and template for detailed blog post as well as CSS for the site and error correction.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from blog import views
+from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
 #name='post_list'는 URL에 이름을 붙인 것으로 뷰를 식별
